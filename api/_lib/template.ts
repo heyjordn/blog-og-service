9695db69aba7d0bb2e55548cb1826e9848aea658
heyjordn <proxima.aust@gmail.com>
2022-12-13 10:28:17 -0500

    feat, added pattern backgrounds

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -14,10 +14,12 @@ const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('ba
 function getCss(theme: string, fontSize: string) {
     let background = 'white';
     let foreground = 'black';
+    let pattern = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 40 40'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%23dfdee1' fill-opacity='0.4'%3E%3Cpath d='M0 38.59l2.83-2.83 1.41 1.41L1.41 40H0v-1.41zM0 1.4l2.83 2.83 1.41-1.41L1.41 0H0v1.41zM38.59 40l-2.83-2.83 1.41-1.41L40 38.59V40h-1.41zM40 1.41l-2.83 2.83-1.41-1.41L38.59 0H40v1.41zM20 18.6l2.83-2.83 1.41 1.41L21.41 20l2.83 2.83-1.41 1.41L20 21.41l-2.83 2.83-1.41-1.41L18.59 20l-2.83-2.83 1.41-1.41L20 18.59z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E";
 
     if (theme === 'dark') {
         background = '#1B1D30';
         foreground = 'white';
+        pattern = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 40 40'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%23454545' fill-opacity='0.4'%3E%3Cpath d='M0 38.59l2.83-2.83 1.41 1.41L1.41 40H0v-1.41zM0 1.4l2.83 2.83 1.41-1.41L1.41 0H0v1.41zM38.59 40l-2.83-2.83 1.41-1.41L40 38.59V40h-1.41zM40 1.41l-2.83 2.83-1.41-1.41L38.59 0H40v1.41zM20 18.6l2.83-2.83 1.41 1.41L21.41 20l2.83 2.83-1.41 1.41L20 21.41l-2.83 2.83-1.41-1.41L18.59 20l-2.83-2.83 1.41-1.41L20 18.59z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E";
     }
     return `
     @font-face {
@@ -43,6 +45,7 @@ function getCss(theme: string, fontSize: string) {
 
     body {
         background: ${background};
+        background-image: url("${pattern}");
         height: 100vh;
         width: 100vw;
         display: flex;
@@ -83,7 +86,7 @@ function getCss(theme: string, fontSize: string) {
         font-size: ${sanitizeHtml(fontSize)};
         font-style: normal;
         color: ${foreground};
-        line-height: 1;
+        line-height: 1.8;
         font-weight: 900;
         margin-left: 50px;
         margin-top: -10px;
@@ -137,7 +140,7 @@ export function getHtml(parsedReq: ParsedRequest) {
             </div>
             <div style="display: flex;align-items: center;">
                 <div>
-                    <img style="height:150px;border-radius: 100%;margin-right: 50px;margin-left: 50px;" src="https://avatars.githubusercontent.com/u/4820517?v=4" alt="heyjordn">            
+                    <img style="height:150px;border-radius: 100%;margin-right: 50px;margin-left: 50px; margin-top:40px;" src="https://avatars.githubusercontent.com/u/4820517?v=4" alt="heyjordn">            
                 </div>
                 <div>
                     <p class="sub-heading">Jordan Jones</p>
